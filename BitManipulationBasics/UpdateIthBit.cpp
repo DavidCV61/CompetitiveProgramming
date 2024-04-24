05ba1dbfbd7a32d94aea99c3dcfdb89535497007
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2024-04-24 15:37:27 -0600

    Create UpdateIthBit.cpp

---
+++ BitManipulationBasics/UpdateIthBit.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int getithbit (int n, int i){
+    int mask=(1<<i);
+    return (n&mask)>0?1:0;
+
+}
+
+void setithbit(int &n, int i){
+    int mask=(1<<i);
+    n=(n|mask);
+
+}
+
+void clearBith(int &n,int i){
+     int mask=~(1<<i);
+    n=(n&mask);
+
+}
+
+void updateithbit(int &n,int i,int v){
+
+    clearBith(n,i);
+    int mask=(v<<i);
+    n=n|mask;
+
+}
+
+int main(){
+int n=13;
+int i;
+cin>>i;
+
+//cout<<getithbit(n,i);
+//setithbit(n,i);
+//clearBith(n,i);
+updateithbit(n,i,1);
+cout<<n<<endl;
+      
+
+    return 0;
+}
