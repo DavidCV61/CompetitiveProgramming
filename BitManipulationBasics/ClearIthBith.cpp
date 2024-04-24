288dd544e5934792bb859ba41a8cf66ba10acec2
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2024-04-24 15:34:06 -0600

    Create ClearIthBith.cpp

---
+++ BitManipulationBasics/ClearIthBith.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int getithbit (int n, int i){
+    int mask=(1<<i);
+    return (n&mask)>0?1:0;
+
+}
+
+void setithbit(int &n, int i){
+    int mask=(1<<i);
+    n=(n|mask);
+
+}
+
+void clearBith(int &n,int i){
+     int mask=~(1<<i);
+    n=(n&mask);
+
+}
+
+int main(){
+int n=5;
+int i;
+cin>>i;
+
+//cout<<getithbit(n,i);
+//setithbit(n,i);
+clearBith(n,i);
+cout<<n<<endl;
+      
+
+    return 0;
+}
