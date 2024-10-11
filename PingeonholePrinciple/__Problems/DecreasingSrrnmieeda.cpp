609684b35f5c3eea8c972c266c6054840efc83e4
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2024-10-10 18:19:24 -0600

    Create DecreasingSrrnmieeda.cpp

---
+++ PingeonholePrinciple/__Problems/DecreasingSrrnmieeda.cpp
@@ -0,0 +1,44 @@
+//Link: https://www.codechef.com/practice/course/2-star-difficulty-problems/DIFF1500/problems/DECREM
+
+
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+
+void solve(){
+
+	ll l,r;
+
+	cin>>l>>r;
+
+	ll m=r/2;
+	
+
+	if(l>m) cout<<r<<endl;
+	else cout<<"-1"<<endl;
+
+
+
+
+
+	
+}
+
+
+int main()
+{
+
+	//for(ll i=0;i<100;i++)cout<<i<<" "<<v[i]<<endl;
+
+	ll t;
+	cin>>t;
+	while(t--) solve();
+
+		
+
+
+
+
+	return 0;
+}
