5577470b77b5425853a8aa5f0e418e2530a98fe5
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2024-11-17 21:42:00 -0600

    Create PrimeFactorisation_O(N).cpp

---
+++ PrimeNumbers&Factorization/PrimeFactorisation_O(N).cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+//Brute Force O(N)
+void factorise(int n){
+
+	for(int i=2; i<=n; i++){
+		if(n%i == 0){
+			int cnt = 0;
+			while(n%i == 0){
+				cnt++;
+				n = n/i;
+			}
+
+			cout<<i<<"^"<<cnt<<",";
+		}
+	}
+}
+
+
+int main(){
+
+	int n;
+	cin>>n;
+
+	factoriseOpt(n);
+
+	return 0;
+}
