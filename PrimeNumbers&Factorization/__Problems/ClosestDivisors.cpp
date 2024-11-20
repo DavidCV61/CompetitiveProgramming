19465b264607a5330353a434407f9ad93b596ccd
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2024-11-19 23:11:40 -0600

    Update ClosestDivisors.cpp

--- PrimeNumbers&Factorization/__Problems/ClosestDivisors.cpp
+++ PrimeNumbers&Factorization/__Problems/ClosestDivisors.cpp
@@ -1,48 +1,42 @@
 class Solution {
 public:
 //Link: https://leetcode.com/problems/closest-divisors/description/
-    vector<int> closestDivisors(int num) {
+      vector<int> closestDivisors(int num) {
 
-        int x=num+1;
-        int y=num+2;
-        int ansx=1;
-        int ansy=1;
-
-        for(int i=sqrt(x);i>1;i--){
-
-            if(x%i==0){
-                ansx=i;
-                break;
-            }
-        }
-         for(int i=sqrt(y);i>1;i--){
-
-            if(y%i==0){
-                ansy=i;
-                break;
-            }
-        }
-
-
-        int compx=abs((x/ansx)-ansx);
-
-        int compy=abs((y/ansy)-ansy);
-        cout<<compy<<" "<<compx<<endl;
-
-        vector <int> v(2);
-
-        if(compx>compy){
-
-            v[0]=y/ansy;
-            v[1]=ansy;
-        }else{
-             v[0]=x/ansx;
-            v[1]=ansx;
-        }
-
-
-        return v;
-
+        int cmp=INT_MAX;
+        vector <int> ans(2);
         
+      for(int i=1;i*i<=(num+1);i++){
+
+            if((num+1)%i==0){
+                int aux=abs(((num+1)/i)-i);
+            if(aux<cmp){
+                ans[0]=(num+1)/i;
+                ans[1]=i;
+                cmp=aux;
+            }
+            }
+
+
+      }
+
+        for(int i=1;i*i<=(num+2);i++){
+
+            if((num+2)%i==0){
+                int aux=abs(((num+2)/i)-i);
+            if(aux<cmp){
+                ans[0]=(num+2)/i;
+                ans[1]=i;
+                cmp=aux;
+            }
+            }
+
+
+      }
+
+      return ans;
+
+
+
     }
 };
