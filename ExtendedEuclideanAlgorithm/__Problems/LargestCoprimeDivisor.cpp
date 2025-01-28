1d5456d62e9d347458515a7c0b9ff3c145fb0443
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2025-01-28 12:18:26 -0600

    Update LargestCoprimeDivisor.cpp

--- ExtendedEuclideanAlgorithm/__Problems/LargestCoprimeDivisor.cpp
+++ ExtendedEuclideanAlgorithm/__Problems/LargestCoprimeDivisor.cpp
@@ -1,24 +1,20 @@
 //Link: https://www.interviewbit.com/problems/largest-coprime-divisor/
 
-int gcd(int a, int b){
 
+int gcd(int a,int b){
+	
 	if(b==0) return a;
 	return gcd(b,a%b);
 }
-
-int solve(int a,int b){
-
-	int num=gcd(a,b);
-
-
-		if(num==1) return a;
-		return solve(a/num,b);
-
-
-
-}
 int Solution::cpFact(int A, int B) {
-    
-    
-    return solve(A,B);
+	int x=gcd(A,B);
+
+	if(x==1) return A;
+	return cpFact(A/x,B);
+
+
 }
+
+
+
+
